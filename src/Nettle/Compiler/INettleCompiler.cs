e3c745a3cae253d143340ce458cc4edfca256a44
Craig Bridges <craig@changedigital.co.uk>
2017-10-24 17:52:53 +0100

    Included XML comments

--- src/Nettle/Compiler/INettleCompiler.cs
+++ src/Nettle/Compiler/INettleCompiler.cs
@@ -9,16 +9,6 @@
     public interface INettleCompiler
     {
         /// <summary>
-        /// Compiles the template specified
-        /// </summary>
-        /// <param name="template">The template to compile</param>
-        /// <returns>An action that will write to a text writer</returns>
-        //Action<TextWriter, object> Compile
-        //(
-        //    TextReader template
-        //);
-
-        /// <summary>
         /// Compiles the template content specified
         /// </summary>
         /// <param name="templateContent">The template content</param>
